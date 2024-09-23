5e98c99c5f32257ac62194d36d185fb21c1480f2
Leandro Luna <leandro.j.luna@gmail.com>
2024-09-22 22:48:06 -0300

    refactor: implement solid principles and unit tests

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,43 +1,10 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-import httpx
-import spacy
-from app.nlp_processor import process_text
-from app.database import store_data
-from slowapi import Limiter
-from slowapi.util import get_remote_address
-from slowapi.errors import RateLimitExceeded
+from fastapi import FastAPI
+from app.api.controllers import router
 from slowapi.middleware import SlowAPIMiddleware
+from app.core.settings import limiter
 
 app = FastAPI()
 
-limiter = Limiter(key_func=get_remote_address)
 app.state.limiter = limiter
-app.add_exception_handler(RateLimitExceeded, lambda request, exc: HTTPException(status_code=429, detail="Rate limit exceeded"))
-
 app.add_middleware(SlowAPIMiddleware)
-
-nlp = spacy.load("en_core_web_sm")
-
-class TextRequest(BaseModel):
-    text: str
-
-@app.post("/process-text")
-@limiter.limit("5/minute")
-async def process_text_endpoint(request: TextRequest):
-    text = request.text
-    entities, mwes = process_text(nlp, text)
-    
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.get("https://httpbin.org/delay/3")
-        except httpx.RequestError:
-            raise HTTPException(status_code=503, detail="External API request failed")
-    
-    store_data(text, entities, mwes)
-    
-    return {
-        "original_text": text,
-        "entities": entities,
-        "mwes": mwes
-    }
+app.include_router(router)
